a5cff461fc2159148796113051c2a89f3c90c929
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-15 19:38:32 +0100

    added asset type and conditionally showing card also inputed name of asset

--- components/Search/SearchContainer.tsx
+++ components/Search/SearchContainer.tsx
@@ -1,7 +1,15 @@
 import SearchForm from "./SearchForm";
 import SearchCard from "./SearchCard";
+import { useState } from "react";
+import { setDatasets } from "react-chartjs-2/dist/utils";
+import { Asset } from "../../types/assetType";
 
 const SearchContainer = () => {
+  const [detailedAsset, setDetailedAsset] = useState<Asset[]>();
+
+  const setDetailedAssetHandler = (val: Asset[]) => {
+    setDetailedAsset(val);
+  };
   return (
     <div className="h-screen w-screen grid place-items-center ">
       <div
@@ -9,8 +17,8 @@ const SearchContainer = () => {
 
 "
       >
-        <SearchForm />
-        <SearchCard />
+        <SearchForm setAsset={setDetailedAssetHandler} />
+        <SearchCard asset={detailedAsset!} />
       </div>
     </div>
   );
