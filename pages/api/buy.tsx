cf6b3098e466a3475baa7867167835bf23094f91
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-16 19:25:17 +0100

    started creating ap route for buying assetss

--- pages/api/buy.tsx
+++ pages/api/buy.tsx
@@ -1,3 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import connectToMongo from "../../helpers/connectToMongo";
+// dodaj logike do kupowania krypto
+const buyAsset = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const db = await connectToMongo();
+  }
+};
 
-const buy = async (req: NextApiRequest, res: NextApiResponse) => {};
+export default buyAsset;
