e96cd2d7c22a34ec16fe724d98a952ffb22d6a29
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-17 18:40:59 +0100

    favorite cryptos are added to mongoDb and then shown on main page

--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -7,15 +7,18 @@ const addCryptoToFavorites = async (
   res: NextApiResponse
 ) => {
   if (req.method === "POST") {
-    const addedAsset = req.body;
+    const addedAsset = req.body as Asset;
     const client = await MongoClient.connect(
       "mongodb+srv://maciejtest:ipT4zxXWbIF4sCqG@cluster0.vv1w4.mongodb.net/stockystocks?retryWrites=true&w=majority"
     );
 
     const db = client.db();
-    console.log(addedAsset);
+    const assetId = {
+      asset_id: addedAsset,
+    };
 
     const detailedAssetsCollection = db.collection("detailedAssets");
+    detailedAssetsCollection.insertOne(assetId);
   }
 };
 
