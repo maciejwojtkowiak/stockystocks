a9c9187bcef45dc308adc33cc274e3b3ea3801c5
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-17 09:47:51 +0100

    data is fetched from database id to coinapi making it dynamic!

--- helpers/getDataFromMongo.tsx
+++ helpers/getDataFromMongo.tsx
@@ -1,12 +1,33 @@
-import { AssetFromDb, TransformedAssetsFromDb } from "../types/assetType";
+import {
+  Asset,
+  AssetFromDb,
+  TransformedAssetsFromDb,
+} from "../types/assetType";
 import connectToMongo from "./connectToMongo";
 
 export const getDataFromMongo = async () => {
   const db = await connectToMongo();
+  const idArray = [] as Asset[];
   const detailedAssetsCollection = db.collection("detailedAssets");
+  const getAsset = async (assetId: string) => {
+    const response = await fetch(
+      `https://rest.coinapi.io/v1/assets/${assetId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CoinAPI-Key": "EB201340-DF56-494E-BA6F-9524985EA13C",
+        },
+      }
+    );
+    const data = await response.json();
+    idArray.push(data);
+  };
   const detailedAssets = (await detailedAssetsCollection
     .find({})
     .toArray()) as AssetFromDb[];
+  detailedAssets.forEach((asset) => getAsset(asset.asset_id));
+
   const correctAsset = detailedAssets.map((asset) => {
     return {
       ...asset,
