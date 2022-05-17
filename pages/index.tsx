730f2b6e337402621e1eac3198d3f3cfb3e3d52b
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-17 13:56:24 +0100

    data is dynamically fetched from database

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,39 +8,28 @@ import { Asset } from "../types/assetType";
 const Home: NextPage = (
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) => {
-  const assetsArray = [] as Asset[];
-  const idsArr = props.assets as string[];
-  idsArr.forEach((asset: string) => {
-    const getAssets = async () => {
-      const response = await fetch(
-        `https://rest.coinapi.io/v1/assets/${asset}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "X-CoinAPI-Key": "EB201340-DF56-494E-BA6F-9524985EA13C",
-          },
-        }
-      );
-      const data = await response.json();
-      assetsArray.push(data);
-    };
-    getAssets();
-  });
-  console.log(assetsArray);
-  console.log("It is an asset" + props.assets);
   return <MainPage asset={props.assets} />;
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  // Po zwroceniu tablicy zrob fetch kryptowaluty dla kazdego i przekaż dalej
   const assetsId = await getDataFromMongo();
+
+  const promises = assetsId.map(async (id: string) => {
+    const response = await fetch(`https://rest.coinapi.io/v1/assets/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CoinAPI-Key": "EB201340-DF56-494E-BA6F-9524985EA13C",
+      },
+    });
+    return await response.json();
+  });
 
   return {
     props: {
-      assets: assetsId,
+      assets: (await Promise.all(promises)).flat(),
     },
-    revalidate: 1,
+    revalidate: 3600,
   };
 };
 
