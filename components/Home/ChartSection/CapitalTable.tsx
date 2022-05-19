8b6c22d494d71f1e6165d600fff51f61cc32a092
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-19 11:49:50 +0100

    added logic for money earned

--- components/Home/ChartSection/CapitalTable.tsx
+++ components/Home/ChartSection/CapitalTable.tsx
@@ -3,24 +3,30 @@ import { RootState } from "../../../store/Store";
 
 const CapitalTable = () => {
   const money = useSelector((state: RootState) => state.assets.balance);
-  console.log(money);
   const boughtAssets = useSelector(
     (state: RootState) => state.assets.boughtAssets
   );
 
+  const totalBalance =
+    money +
+    boughtAssets.reduce(
+      (acc, cur) => (acc + cur.asset.price_usd) * cur.quantity,
+      0
+    );
+
   return (
     <div className=" mt-16 grid grid-cols-capital-fill gap-2 mx-2 items-center">
       <p className="text-center text-center bg-green-300 p-2">
-        Revenue <span className="block"></span>
+        Revenue <span className="block">{money}</span>
       </p>
       <p className="text-center bg-green-300 p-2">
-        Money earned <span className="block">1000</span>
+        Money earned <span className="block">{+totalBalance - +money}</span>
       </p>
       <p
         className="text-center bg-green-300 p-2
       "
       >
-        Money spent <span className="block">1000</span>
+        Money spent <span className="block">{totalBalance}</span>
       </p>
     </div>
   );
