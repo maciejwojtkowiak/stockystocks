fd90378ba79cc17ea9eb49885d63734b51d17d08
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-19 14:21:50 +0100

    passed data to search asset

--- components/UI/DetailedAssetCard.tsx
+++ components/UI/DetailedAssetCard.tsx
@@ -48,82 +48,45 @@ const DetailedAssetCard: React.FC<funcProps> = (props) => {
   };
 
   // favs cards
-  if (!props.isSearched && props.asset) {
-    return (
-      <React.Fragment>
-        <div className=" h-full bg-gray-100 rounded-lg grid grid-cols-auto-full  drop-shadow-2xl relative  ">
-          <div className="w-32 h-32 rounded-full grid self-center   ">
-            <Image src={Btc} alt="" />
-          </div>
-          <div className=" grid grid-cols-fill-40 place-items-center grid-rows-2 auto-rows-min my-6 text-center">
-            <DetailedAssetCardData asset={props.asset} />
-            <div className="lg:col-start-3 text-2xl">
-              <button
-                onClick={onShowFormHandler}
-                className="shadow-lg bg-green-400 shadow-green-500/50 px-4 py-1 mr-4"
-              >
-                Buy
-              </button>
-              <button className="shadow-lg bg-red-500 shadow-red-500/50 px-4 py-1">
-                Sell
-              </button>
-            </div>
-          </div>
-          <button onClick={onDeleteHandler}>
-            <FaTrash className="absolute top-3 right-5" />
-          </button>
-          {formIsShown && <BuyForm asset={props.asset} />}
-        </div>
-      </React.Fragment>
-    );
-  }
 
-  // It is card when user searched
   return (
     <React.Fragment>
-      {props.isSearched && (
-        <div className=" h-full bg-gray-100 rounded-lg grid grid-cols-auto-full  drop-shadow-2xl  ">
-          <div className="w-32 h-32 rounded-full grid self-center   ">
-            <Image src={Btc} alt="" />
-          </div>
-          <div className=" grid grid-cols-fill-40 place-items-center grid-rows-2 auto-rows-min my-6 text-center">
-            <p>
-              Hello
-              <span className="block ">
-                {props.asset !== undefined ? props.asset.name : ""}
-              </span>
-            </p>
-            <p>
-              Hello <span className="block ">123</span>
-            </p>
-            <p>
-              Hello <span className="block ">123</span>
-            </p>
-            <p>
-              Hello <span className="block  ">123</span>
-            </p>
-            <p>
-              Hello <span className="block ">123</span>
-            </p>
-            <p>
-              Hello <span className="block ">123</span>
-            </p>
-            <div className="lg:col-start-3 text-2xl">
-              <button className="shadow-lg bg-green-400 shadow-green-500/50 px-4 py-1 mr-4">
-                Buy
-              </button>
-              <button className="shadow-lg bg-red-500 shadow-red-500/50 px-4 py-1">
-                Sell
-              </button>
-              <button
-                onClick={onAddToFavHandler}
-                className="shadow-lg bg-blue-500 shadow-blue-500/50 px-4 py-1"
-              >
-                Add to fav
-              </button>
+      {props.asset && (
+        <React.Fragment>
+          <div className=" h-full bg-gray-100 rounded-lg grid grid-cols-auto-full  drop-shadow-2xl relative  ">
+            <div className="w-32 h-32 rounded-full grid self-center   ">
+              <Image src={Btc} alt="" />
             </div>
+            <div className=" grid grid-cols-fill-40 place-items-center grid-rows-2 auto-rows-min my-6 text-center">
+              <DetailedAssetCardData asset={props.asset!} />
+              <div className="lg:col-start-3 text-2xl">
+                <button
+                  onClick={onShowFormHandler}
+                  className="shadow-lg bg-green-400 shadow-green-500/50 px-4 py-1 mr-4"
+                >
+                  Buy
+                </button>
+                <button className="shadow-lg bg-red-500 shadow-red-500/50 px-4 py-1">
+                  Sell
+                </button>
+                {props.isSearched && (
+                  <button
+                    onClick={onAddToFavHandler}
+                    className="shadow-lg bg-blue-500 shadow-blue-500/50 px-4 py-1"
+                  >
+                    Add to fav
+                  </button>
+                )}
+              </div>
+            </div>
+            <button onClick={onDeleteHandler}>
+              {!props.isSearched && (
+                <FaTrash className="absolute top-3 right-5" />
+              )}
+            </button>
+            {formIsShown && <BuyForm asset={props.asset!} />}
           </div>
-        </div>
+        </React.Fragment>
       )}
     </React.Fragment>
   );
