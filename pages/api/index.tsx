fded662159bdf1f8f0d6736ede7438927f0ba0a9
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-24 22:30:52 +0100

    add logic that if item is already bought it increases its quantity

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -8,15 +8,26 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const boughtAsset = req.body as BoughtAsset;
     const db = await connectToMongo();
     const assetCollection = db.collection("boughtAssets");
-    const isTheSameAssetsExist = await assetCollection
+    const isTheSameAssetsExist = (await assetCollection
       .find({ "asset.asset_id": boughtAsset.asset.asset_id })
-      .toArray();
-    console.log(isTheSameAssetsExist);
-    const boughtAssetModel = {
-      asset: boughtAsset.asset,
-      quantity: boughtAsset.quantity,
-    };
-    await assetCollection.insertOne(boughtAssetModel);
+      .toArray()) as BoughtAsset[];
+
+    if (isTheSameAssetsExist.length > 0) {
+      assetCollection.updateOne(
+        { "asset.asset_id": boughtAsset.asset.asset_id },
+        {
+          $set: {
+            quantity: isTheSameAssetsExist[0].quantity + boughtAsset.quantity,
+          },
+        }
+      );
+    } else {
+      const boughtAssetModel = {
+        asset: boughtAsset.asset,
+        quantity: boughtAsset.quantity,
+      };
+      await assetCollection.insertOne(boughtAssetModel);
+    }
   }
 
   if (req.method === "DELETE") {
