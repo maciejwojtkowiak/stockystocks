94e2635fef602ab396dcb4292196bd59a72405e0
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-05-28 12:26:11 +0100

    fixed capital table

--- components/Home/ChartSection/CapitalTable.tsx
+++ components/Home/ChartSection/CapitalTable.tsx
@@ -21,16 +21,16 @@ const CapitalTable = () => {
   return (
     <div className=" mt-16 grid grid-cols-capital-fill gap-2 mx-2 items-center">
       <p className="text-center text-center bg-green-300 p-2">
-        Revenue <span className="block">{money}</span>
+        Revenue <span className="block">{money.toFixed(2)}$</span>
       </p>
       <p className="text-center bg-green-300 p-2">
-        Money earned <span className="block">{totalBalance}</span>
+        Money in Assets<span className="block">{total.toFixed(2)}$</span>
       </p>
       <p
         className="text-center bg-green-300 p-2
       "
       >
-        Money spent <span className="block">{totalBalance}</span>
+        Total capital <span className="block">{totalBalance}$</span>
       </p>
     </div>
   );
