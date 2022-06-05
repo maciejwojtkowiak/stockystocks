e37a34f9e181010f29c14f639b33cf0700b2ddf4
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-05 16:23:45 +0200

    created logic for fetching actual bought assets

--- components/Home/ChartSection/CapitalTable.tsx
+++ components/Home/ChartSection/CapitalTable.tsx
@@ -8,33 +8,35 @@ const CapitalTable = () => {
   const [actualBoughtAssetsList, setActualBoughtAssetsList] = useState<Asset[]>(
     []
   );
-  console.log(actualBoughtAssetsList);
+
   const money = +useSelector((state: RootState) => state.assets.balance);
   const boughtAssets = useSelector(
     (state: RootState) => state.assets.boughtAssets
   );
-  const boughtIds = [] as string[];
-  boughtAssets.map((asset) => boughtIds.push(asset.asset.asset_id));
-  const actualBoughtAssets = boughtIds.map(async (id: string) => {
-    const response = await fetch(`https://rest.coinapi.io/v1/assets/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CoinAPI-Key": "EB201340-DF56-494E-BA6F-9524985EA13C",
-      },
+  const getActualBoughtAssets = () => {
+    const boughtIds = [] as string[];
+    boughtAssets.map((asset) => boughtIds.push(asset.asset.asset_id));
+    const fetchActualBoughtAsset = boughtIds.map(async (id: string) => {
+      const response = await fetch(`https://rest.coinapi.io/v1/assets/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CoinAPI-Key": "EB201340-DF56-494E-BA6F-9524985EA13C",
+        },
+      });
+
+      return await response.json();
     });
-    return await response.json();
-  });
-  const getActualBoughtAssets = async () => {
-    const data = await Promise.all(actualBoughtAssets);
-    console.log("data");
+
+    const data = Promise.all(fetchActualBoughtAsset);
     console.log(data);
-    setActualBoughtAssetsList(data);
   };
 
   useEffect(() => {
-    getActualBoughtAssets();
+    console.log("actual");
+    console.log(getActualBoughtAssets());
   }, []);
+  console.log("actual");
 
   const total = +actualBoughtAssetsList.reduce((acc, cur) => {
     const foundBoughtAsset = boughtAssets.find(
